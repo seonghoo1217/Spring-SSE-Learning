0462eacfed5eb69c8b180908df47bfa0e273aac0
seonghoo1217 <seonghoo1217@naver.com>
2023-07-24 14:28:17 +0900

    feat : add a NotFoundException By User Id

---
+++ SSE/src/main/java/com/example/sse/handler/controller/ExceptionHadnlerController.java
@@ -0,0 +1,15 @@
+package com.example.sse.handler.controller;
+
+import com.example.sse.handler.exception.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHadnlerController {
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+}
