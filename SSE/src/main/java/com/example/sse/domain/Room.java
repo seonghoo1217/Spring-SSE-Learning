ea25b13240b02564331c0408fae38818ab940d62
seonghoo1217 <seonghoo1217@naver.com>
2023-07-27 14:22:58 +0900

    feat : add Domain Room

---
+++ SSE/src/main/java/com/example/sse/domain/Room.java
@@ -0,0 +1,24 @@
+package com.example.sse.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long roomSeq;
+
+    private String roomName;
+
+    private String roomCode;
+}
