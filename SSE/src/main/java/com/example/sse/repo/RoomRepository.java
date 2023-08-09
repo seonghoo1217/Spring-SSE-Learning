de927a4579a5165b86c5ba35bf428e243426c343
seonghoo1217 <seonghoo1217@naver.com>
2023-08-10 00:33:19 +0900

    refactor : eliminate domain purpose for only test sse

--- SSE/src/main/java/com/example/sse/repo/RoomRepository.java
+++
@@ -1,9 +0,0 @@
-/*
-package com.example.sse.repo;
-
-import com.example.sse.domain.Room;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomRepository extends JpaRepository<Room,Long> {
-}
-*/
